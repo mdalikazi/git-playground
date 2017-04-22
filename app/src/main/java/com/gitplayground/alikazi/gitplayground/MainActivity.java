4b028f9730ecac99b5141ad3c7c773b30d253c8a
Ali Kazi <akazi@deputy.com>
2017-04-22 13:37:06 +1000

    Feature 1 complete

--- app/src/main/java/com/gitplayground/alikazi/gitplayground/MainActivity.java
+++ app/src/main/java/com/gitplayground/alikazi/gitplayground/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mTextView = (TextView) findViewById(R.id.content_main_text_view);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
+                mTextView.setText("Bye World");
             }
         });
+
     }
 
     @Override
